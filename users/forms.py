4a6d305f90813369b6342a579d7cb8c7bb6f2b13
developerNihad <nihadnahmatli@gmail.com>
2025-03-28 20:06:56 +0400

    change

---
+++ users/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'bio', 'location')
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already exist.")
+
+        return email
+    
+
+class CustomUserChangeForm(forms.ModelForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'bio', 'location')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError("This email already in use.")
+        
+        return email
+    
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    info = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=3000, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
