e7f561ad520d1d9276bf3a80052cf8f413cff8ce
developerNihad <nihadnahmatli@gmail.com>
2025-03-31 11:38:59 +0400

    updated

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+class CustomUserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'bio', 'location')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'bio', 'location'),
+        }),
+    )
+
+
+    list_display = ('username', 'email', 'bio', 'location', 'is_staff')
+    search_fields = ('username', 'email', 'bio', 'location')
+    ordering = ('username',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm, CustomUserChangeForm, CustomAuthenticationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'user': request.user})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
