3d5b1d52ff6663f9c54b9b6527e08329c11c9e36
developerNihad <nihadnahmatli@gmail.com>
2025-04-01 21:37:45 +0400

    updated

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authenticate, logout
-from .forms import CustomUserCreationForm, CustomUserChangeForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login, authenticate, logout
+from .forms import CustomUserCreationForm, CustomUserChangeForm, AuthenticationForm
 from django.contrib import messages
 
 
@@ -24,3 +24,19 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password!')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'users/login.html', {'form': form})
